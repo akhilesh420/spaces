a051892f28e38c75513cc909c4fffab2f7621b7a
akhilesh420 <72787785+akhilesh420@users.noreply.github.com>
2021-06-28 19:37:56 +0530

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HowItWorksComponent } from './home/how-it-works/how-it-works.component';
+import { BenefitsComponent } from './home/benefits/benefits.component';
+import { MissionStatementComponent } from './home/mission-statement/mission-statement.component';
+import { CollectionComponent } from './home/how-it-works/collection/collection.component';
+import { CreateComponent } from './home/how-it-works/create/create.component';
+import { ShareComponent } from './home/how-it-works/share/share.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HowItWorksComponent,
+    BenefitsComponent,
+    MissionStatementComponent,
+    CollectionComponent,
+    CreateComponent,
+    ShareComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
