99ef4a3e0a93f9222e5dbff38dc8493262127f77
akhilesh420 <72787785+akhilesh420@users.noreply.github.com>
2021-07-06 20:28:21 +0530

    collection animtions done

--- src/app/home/how-it-works/collection/collection.component.ts
+++ src/app/home/how-it-works/collection/collection.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-collection',
   templateUrl: './collection.component.html',
   styleUrls: ['./collection.component.css']
 })
-export class CollectionComponent implements OnInit {
+export class CollectionComponent implements OnInit, AfterViewInit {
+
+  count = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    setInterval(() => {
+      this.count = (this.count + 1) % 3
+    }, 3000);
   }
 
 }
