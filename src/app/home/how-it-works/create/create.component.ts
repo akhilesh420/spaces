e2ed3a53924686d56d27f5a43caa5d5b9aba53e6
akhilesh420 <72787785+akhilesh420@users.noreply.github.com>
2021-07-07 22:58:59 +0530

    create

--- src/app/home/how-it-works/create/create.component.ts
+++ src/app/home/how-it-works/create/create.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css']
 })
-export class CreateComponent implements OnInit {
+export class CreateComponent implements OnInit, AfterViewInit {
+
+  count = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    setInterval(() => {
+      this.count = (this.count + 1) % 3
+    }, 3000);
   }
 
 }
