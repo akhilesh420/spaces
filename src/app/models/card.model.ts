50c243ea60fea00c61ef50bdef5fbec91a8ced1f
akhilesh420 <72787785+akhilesh420@users.noreply.github.com>
2021-09-03 14:38:40 -0500

    writing js

---
+++ src/app/models/card.model.ts
@@ -0,0 +1,17 @@
+export class Card {
+
+  constructor(
+    public cardNumber: number,
+    public tile: string,
+    public subtitle: string,
+    public subCard_1: SubCard
+  ) {}
+}
+
+export class SubCard {
+
+  constructor(
+    public animation: string,
+    public information: string
+  ){}
+}
