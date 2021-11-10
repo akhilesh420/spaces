31bf5f760b9f060f697e12396db53800d46cc0c1
akhilesh420 <72787785+akhilesh420@users.noreply.github.com>
2021-11-09 19:34:57 -0600

    deleted everything

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,9 @@
-import { MessageComponent } from './sign-up/message/message.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
-  {path: 'signup', component: SignUpComponent},
-  {path: 'message', component: MessageComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
 @NgModule({
