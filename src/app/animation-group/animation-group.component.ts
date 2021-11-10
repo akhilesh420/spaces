c73c234fc05271d4b1ade17914d5533e57f4de85
akhilesh420 <72787785+akhilesh420@users.noreply.github.com>
2021-11-10 17:07:12 -0600

    added segentaion to lottie animations

--- src/app/animation-group/animation-group.component.ts
+++ src/app/animation-group/animation-group.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit, AfterViewInit } from '@angular/core';
 import { AnimationItem } from 'lottie-web';
 import { AnimationOptions } from 'ngx-lottie';
 
@@ -9,21 +9,38 @@ import { AnimationOptions } from 'ngx-lottie';
 })
 export class AnimationGroupComponent implements OnInit {
 
-  tiktok: AnimationOptions = {path: 'assets/Lottie animations/Tiktok/tiktok-3713691.json',};
+  private intialSegment: [number, number] = [30, 90];
+  private segment: [number, number] = [30, 90];
 
-  pinterest: AnimationOptions = {path: 'assets/Lottie animations/Pinterest/pinterest-3713687.json',};
+  pinterest: AnimationOptions = {path: 'assets/Lottie animations/Pinterest/pinterest-3713687.json',initialSegment: this.intialSegment, autoplay: false, loop: false};
+  instagram: AnimationOptions = {path: 'assets/Lottie animations/Instagram/instagram-3713696.json',initialSegment: this.intialSegment, autoplay: false, loop: false};
+  twitter: AnimationOptions = {path: 'assets/Lottie animations/Twitter/twitter-3713693.json', initialSegment: this.intialSegment, autoplay: false, loop: false};
+  youtube: AnimationOptions = {path: 'assets/Lottie animations/Youtube/youtube-3713694.json', initialSegment: this.intialSegment, autoplay: false, loop: false};
+  behance: AnimationOptions = {path: 'assets/Lottie animations/Behance/behance-3713689.json', initialSegment: this.intialSegment, autoplay: false, loop: false};
+  tiktok: AnimationOptions = {path: 'assets/Lottie animations/Tiktok/tiktok-3713691.json', initialSegment: this.segment, autoplay: true, loop: false}; 
 
-  instagram: AnimationOptions = {path: 'assets/Lottie animations/Instagram/instagram-3713696.json', };
 
-  twitter: AnimationOptions = {path: 'assets/Lottie animations/Twitter/twitter-3713693.json', };
- 
-  youtube: AnimationOptions = {path: 'assets/Lottie animations/Youtube/youtube-3713694.json', };
+  private animationItems: {name: string, animationItem: AnimationItem}[] = [];
+  private counter = 0;
 
-  behance: AnimationOptions = {path: 'assets/Lottie animations/Behance/behance-3713689.json',};
-
-  constructor() { }
+  constructor(private ngZone: NgZone) { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+
+  }
+
+  animationCreated(animationItem: AnimationItem, name: string): void {
+    this.animationItems.push({name: name, animationItem: animationItem});
+  }
+
+  onComplete(event) {
+    this.counter = (this.counter + 1) % this.animationItems.length;
+    console.log(this.counter);
+    this.ngZone.runOutsideAngular(() => {
+      this.animationItems[this.counter].animationItem.playSegments(this.segment, true);
+    });
+  }
 }
