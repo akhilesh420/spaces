89eec657650625f9427e4f6c0d10d3f537af5ef3
akhilesh420 <72787785+akhilesh420@users.noreply.github.com>
2021-11-09 19:33:36 -0600

    delted everything

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { HowItWorksComponent } from './home/how-it-works/how-it-works.component';
-import { BenefitsComponent } from './home/benefits/benefits.component';
-import { MissionStatementComponent } from './home/mission-statement/mission-statement.component';
-import { CollectionComponent } from './home/how-it-works/collection/collection.component';
-import { CreateComponent } from './home/how-it-works/create/create.component';
-import { ShareComponent } from './home/how-it-works/share/share.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { SignUpBannerComponent } from './home/sign-up-banner/sign-up-banner.component';
-import { IntroComponent } from './intro/intro.component';
-import { MessageComponent } from './sign-up/message/message.component';
 import { FormsModule } from '@angular/forms';
 import { TitleCaseDirective } from './directives/title-case.directive';
 import { LoadingComponent } from './loading/loading.component';
@@ -24,26 +13,13 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireAnalyticsModule, ScreenTrackingService, UserTrackingService  } from '@angular/fire/analytics';
 import { ViewTraceDirective } from './directives/view-trace.directive';
-import { BannerComponent } from './banners/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    HowItWorksComponent,
-    BenefitsComponent,
-    MissionStatementComponent,
-    CollectionComponent,
-    CreateComponent,
-    ShareComponent,
-    SignUpComponent,
-    SignUpBannerComponent,
-    IntroComponent,
-    MessageComponent,
     TitleCaseDirective,
     LoadingComponent,
     ViewTraceDirective,
-    BannerComponent,
   ],
   imports: [
     BrowserModule,
