cdd8604328f4196e5585190c8fdb5d09b9a0d827
akhilesh420 <72787785+akhilesh420@users.noreply.github.com>
2021-11-10 19:29:53 -0600

    removed segmentation

--- src/app/animation-group/animation-group.component.ts
+++ src/app/animation-group/animation-group.component.ts
@@ -10,18 +10,26 @@ import { AnimationOptions } from 'ngx-lottie';
 export class AnimationGroupComponent implements OnInit {
 
   private intialSegment: [number, number] = [0, 90];
-  private segment: [number, number] = [0, 90];
+  private segment: [number, number] = [30, 90];
 
-  pinterest: AnimationOptions = {path: 'assets/Lottie animations/Pinterest/pinterest-3713687.json',initialSegment: this.intialSegment, autoplay: false, loop: false};
-  instagram: AnimationOptions = {path: 'assets/Lottie animations/Instagram/instagram-3713696.json',initialSegment: this.intialSegment, autoplay: false, loop: false};
-  twitter: AnimationOptions = {path: 'assets/Lottie animations/Twitter/twitter-3713693.json', initialSegment: this.intialSegment, autoplay: false, loop: false};
-  youtube: AnimationOptions = {path: 'assets/Lottie animations/Youtube/youtube-3713694.json', initialSegment: this.intialSegment, autoplay: false, loop: false};
-  behance: AnimationOptions = {path: 'assets/Lottie animations/Behance/behance-3713689.json', initialSegment: this.intialSegment, autoplay: false, loop: false};
-  tiktok: AnimationOptions = {path: 'assets/Lottie animations/Tiktok/tiktok-3713691.json', initialSegment: this.segment, autoplay: true, loop: false}; 
+  pinterest: AnimationOptions = {path: 'assets/Lottie animations/Pinterest/pinterest-3713687.json'};
+  instagram: AnimationOptions = {path: 'assets/Lottie animations/Instagram/instagram-3713696.json'};
+  twitter: AnimationOptions = {path: 'assets/Lottie animations/Twitter/twitter-3713693.json'};
+  youtube: AnimationOptions = {path: 'assets/Lottie animations/Youtube/youtube-3713694.json'};
+  behance: AnimationOptions = {path: 'assets/Lottie animations/Behance/behance-3713689.json'};
+  tiktok: AnimationOptions = {path: 'assets/Lottie animations/Tiktok/tiktok-3713691.json'}; 
+
+  // pinterest: AnimationOptions = {path: 'assets/Lottie animations/Pinterest/pinterest-3713687.json',initialSegment: this.intialSegment, autoplay: false, loop: false};
+  // instagram: AnimationOptions = {path: 'assets/Lottie animations/Instagram/instagram-3713696.json',initialSegment: this.intialSegment, autoplay: false, loop: false};
+  // twitter: AnimationOptions = {path: 'assets/Lottie animations/Twitter/twitter-3713693.json', initialSegment: this.intialSegment, autoplay: false, loop: false};
+  // youtube: AnimationOptions = {path: 'assets/Lottie animations/Youtube/youtube-3713694.json', initialSegment: this.intialSegment, autoplay: false, loop: false};
+  // behance: AnimationOptions = {path: 'assets/Lottie animations/Behance/behance-3713689.json', initialSegment: this.intialSegment, autoplay: false, loop: false};
+  // tiktok: AnimationOptions = {path: 'assets/Lottie animations/Tiktok/tiktok-3713691.json', initialSegment: this.intialSegment, autoplay: true, loop: false}; 
 
 
   private animationItems: {name: string, animationItem: AnimationItem}[] = [];
   private counter = 0;
+  private count = 0;
 
   constructor(private ngZone: NgZone) { }
 
@@ -34,13 +42,20 @@ export class AnimationGroupComponent implements OnInit {
 
   animationCreated(animationItem: AnimationItem, name: string): void {
     this.animationItems.push({name: name, animationItem: animationItem});
+    // animationItem.setSpeed(2);
   }
 
   onComplete(event) {
-    this.counter = (this.counter + 1) % this.animationItems.length;
-    console.log(this.counter);
+    ++this.counter;
+    const index = this.counter % this.animationItems.length;
     this.ngZone.runOutsideAngular(() => {
-      this.animationItems[this.counter].animationItem.playSegments(this.segment, true);
+      const segment = this.counter < this.animationItems.length ? this.intialSegment : this.segment;
+      this.animationItems[index].animationItem.playSegments(segment, true);
     });
+  }
+
+  onFrameChange(event) {
+    ++this.count;
+    console.log(this.count);
   }
 }
